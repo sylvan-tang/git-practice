dfa3d143850265efe4caa3a762ebdc3a1c7ae1ef
sylvan <sylvan2future@gmail.com>
2023-07-04 11:35:37 +0800

    Bugfix: disable debug message. Ref: #4

--- git-hooks/modules/pull-newest-branch.sh
+++ git-hooks/modules/pull-newest-branch.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-set -ex
-branch_config_name=$1
+set -e
+
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 echo "current branch is $current_branch"
 
 # make sure the branch is at the newest commit
-base_branch=$(git config --list | grep $branch_config_name | tail -n 1 | cut -d '=' -f 2)
+base_branch=$(git config --list | grep $1 | tail -n 1 | cut -d '=' -f 2)
 
-git fetch origin $base_branch:$base_branch
+git checkout $base_branch
+git branch --set-upstream-to=origin/$base_branch $base_branch
+git config pull.rebase true
+git pull
 
 if [[ "${current_branch}" != "${base_branch}" ]]; then
+  git checkout $current_branch
   git rebase $base_branch
 fi
